a0eb7e75926ef59322a20efdfaae590b8ee23c61
Gabriel Samoylov <gsamo122@gmail.com>
2024-10-14 02:03:53 +0300

    adding loading screen

--- components/3d/ViewCanvas.tsx
+++ components/3d/ViewCanvas.tsx
@@ -1,32 +1,44 @@
 "use client";
-import { Perf } from "r3f-perf";
+// import { Perf } from "r3f-perf";
+import { Suspense } from "react";
+import dynamic from "next/dynamic";
 import { View } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+
+const Loader = dynamic(
+  () => import("@react-three/drei").then((mod) => mod.Loader),
+  { ssr: false },
+);
 
 // type Props = {};
 
 // export default function ViewCanvas({}: Props) {
 export default function ViewCanvas() {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        left: "50%",
-        transform: "translateX(-50%)",
-        overflow: "hidden",
-        pointerEvents: "none",
-        zIndex: 30,
-      }}
-      shadows
-      dpr={[1, 1.5]}
-      gl={{ antialias: true }}
-      camera={{
-        fov: 30,
-      }}
-    >
-      <View.Port />
-      {/* <Perf /> */}
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: "50%",
+          transform: "translateX(-50%)",
+          overflow: "hidden",
+          pointerEvents: "none",
+          zIndex: 30,
+        }}
+        shadows
+        dpr={[1, 1.5]}
+        gl={{ antialias: true }}
+        camera={{
+          fov: 30,
+        }}
+      >
+        <Suspense fallback={null}>
+          <View.Port />
+          {/* <Perf /> */}
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
   );
 }
