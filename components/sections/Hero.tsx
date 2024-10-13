dbb43ecd3d0f9b10d54083fc983e8c0bd137d184
Gabriel Samoylov <gsamo122@gmail.com>
2024-10-13 21:28:08 +0300

    animating the hero section

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import gsap from "gsap";
 import Image from "next/image";
+import { useGSAP } from "@gsap/react";
+import ScrollTrigger from "gsap/ScrollTrigger";
 
 import Button from "@/components/Button";
 import { Container } from "@/components/Container";
@@ -7,8 +12,55 @@ import { TextSplitter } from "@/components/TextSplitter";
 import heroImage from "@/public/images/all-cans-bunched.png";
 
 export default function Hero() {
+  gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+  useGSAP(() => {
+    const introTl = gsap.timeline();
+
+    introTl
+      .set(".hero", { opacity: 1 })
+      .from(".hero-header-word", {
+        scale: 3,
+        opacity: 0,
+        ease: "power4.in",
+        delay: 0.3,
+        stagger: 1,
+      })
+      .from(".hero-subheading", { opacity: 0, y: 30 }, "+=.8")
+      .from(".hero-body", { opacity: 0, y: 10 })
+      .from(".hero-button", { opacity: 0, y: 10, duration: 0.6 });
+
+    const scrollTl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom bottom",
+        scrub: 1.5,
+        markers: true,
+      },
+    });
+
+    scrollTl
+      .fromTo(
+        "body",
+        { backgroundColor: "#FDE047" },
+        { backgroundColor: "#D9F99D", overwrite: "auto" },
+        1,
+      )
+      .from(".text-side-heading .split-char", {
+        scale: 0.3,
+        y: 40,
+        rotate: -25,
+        opacity: 0,
+        stagger: 0.1,
+        ease: "back.out(3)",
+        duration: 0.5,
+      })
+      .from(".text-side-body", { y: 20, opacity: 0 });
+  }, {});
+
   return (
-    <Container className="hero">
+    <Container className="hero opacity-0">
       <div className="grid">
         <div className="grid h-screen place-items-center">
           <div className="grid auto-rows-min place-items-center text-center">
